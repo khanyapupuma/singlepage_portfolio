b45544178aa6825548eb1d96d7daf7d5151f568c
khanyapupuma <khalilpupuma@gmail.com>
2024-06-26 11:44:03 +0200

    Update

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="container-fluid">
+      <div class="row lead">
+        <p>Khanya's portfolio &copy; 
+            <span class="shadow">{{ cYear }}</span>
+        </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+name: "FooterComp",
+data(){
+return{
+ cYear: new Date().getUTCFullYear()
+
+}
+}
+}
+</script>
+
+<style lang="sss" scoped>
+
+</style>
